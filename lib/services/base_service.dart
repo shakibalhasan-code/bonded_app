4cd4a6231c773f5775d932c263b6d54332115a4e
Shakib Al Hasan <mdshakib@joinventureai.com>
2026-04-11 15:12:00 +0600

    chore: Clean up whitespace in BaseService class

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class BaseService extends GetxService {
   // Add common service initialization logic here
-  
+
   Future<BaseService> init() async {
     // Perform any necessary async initialization
     return this;
